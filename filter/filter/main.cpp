ac72bc3292c094f039eff9d05524375b19238170
rodimkov <rodimkov@bk.ru>
2023-06-16 19:50:57 +0300

    added command line argument processing

--- filter/filter/main.cpp
+++ filter/filter/main.cpp
@@ -1,25 +1,121 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-
-#include "Filter.h"
 
 using namespace std;
 using namespace cv;
 
-int main()
+const char* helper =
+"\t<image_path> - file name contained the source image, must be 3 - channel, RGB - image\n\
+\t<kernel_path> - file in txt format that stores the convolution kernel\n\
+\t<x> - anchor.x\n\
+\t<y> - anchor.y\n\
+\t<border_type> - border handling.\n\
+\t<output_path> - output file name..\n\
+";
+
+
+int proccesArgument(int argc, char* argv[], string& image_path, string& kernel_path, int& x, int& y, int& border_type, string& output_path);
+Mat vector_to_mat(vector<vector<float>> kernel);
+Mat parse_kernel_from_txt(string kernel_path);
+void filter(Mat& image, Mat& dst, Mat& kernel, Point anchor, int border_type, string output_name);
+
+
+int main(int argc, char* argv[])
 {
-	Mat image = Mat::zeros(500, 500, CV_8UC3);
-	circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
-	circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
+	string image_path, kernel_path, output_path;
+	int x, y, border_type;
+	if (proccesArgument(argc, argv, image_path, kernel_path, x, y, border_type, output_path) != 0)
+	{
+		cout << helper << endl;
+		return 1;
+	}
 
-	int kernel_size = 3;
-	Mat kernel = Mat::ones(kernel_size, kernel_size, CV_32F) / (float)(kernel_size * kernel_size);
+	Point anchor(x, y);
 
-	int border_type = BORDER_CONSTANT;
+	Mat image = imread(image_path, IMREAD_COLOR);
+	Mat dst;
+	Mat kernel = parse_kernel_from_txt(kernel_path);
 
-	Filter filter = Filter();
-	filter.experiment(image, kernel, border_type, "result.png");
+	filter(image, dst, kernel, anchor, border_type, output_path);
+	imwrite(output_path, dst);
+}
+
+int proccesArgument(int argc, char* argv[], string& image_path, string& kernel_path, int& x, int& y, int& border_type, string& output_path)
+{
+	if (argc < 7)
+	{
+		return 1;
+	}
+
+	image_path = argv[1];
+	kernel_path = argv[2];
+	x = atoi(argv[3]);
+	y = atoi(argv[4]);
+	border_type = atoi(argv[5]);
+	output_path = argv[6];
+
+	return 0;
+}
+
+Mat vector_to_mat(vector<vector<float>> kernel)
+{
+	Mat opencv_kernel(kernel.size(), kernel[0].size(), CV_32F);
+
+	for (int i = 0; i < kernel.size(); i++)
+	{
+		for (int j = 0; j < kernel[i].size(); j++)
+		{
+			opencv_kernel.at<float>(i, j) = kernel[i][j];
+		}
+	}
+	return opencv_kernel;
+}
+
+
+Mat parse_kernel_from_txt(string kernel_path)
+{
+	fstream newfile;
+	std::stringstream stream("");
+
+	vector<vector<float>> kernel;
+
+	vector<float> row;
+	string file_line;
+	float value;
+
+	newfile.open(kernel_path, ios::in);
+	if (newfile.is_open())
+	{
+		while (getline(newfile, file_line))
+		{
+			stream.clear();
+			stream.str(file_line);
+
+			while (stream >> value)
+				row.push_back(value);
+
+			kernel.push_back(row);
+			row.clear();
+		}
+		newfile.close();
+	}
+
+	Mat opencv_kernel = vector_to_mat(kernel);
+	return opencv_kernel;
+}
+
+
+void filter(Mat& image, Mat& dst, Mat& kernel, Point anchor, int border_type, string output_name)
+{
+	auto start = std::chrono::steady_clock::now();
+
+	filter2D(image, dst, -1, kernel, anchor, 0, border_type);
+
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 }
