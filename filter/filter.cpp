c9e88c63823aa84bbc0e4ee126aa0c54deaa670c
rodimkov <rodimkov@bk.ru>
2023-06-19 15:41:41 +0300

    formatting fixed

--- filter/filter.cpp
+++ filter/filter.cpp
@@ -10,10 +10,10 @@ using namespace cv;
 
 const char* helper =
 "./filter <image_path> <kernel_path> <x> <y> <border_type> <output_path>\
-\t<image_path> is a file name contained the source image (3-channel, RGB)\n\
-\t<kernel_path> is a file name in the txt-format that stores the kernel\n\
-\t<x> is an anchor x coordinate\n\
-\t<y> is an anchor x coordinate\n\
+\t<image_path> is a file name contained the source image (3-channel, RGB).\n\
+\t<kernel_path> is a file name in the txt-format that stores the kernel.\n\
+\t<x> is an anchor x coordinate.\n\
+\t<y> is an anchor x coordinate.\n\
 \t<border_type> is a border type: 0 = BORDER_CONSTANT, 1 = BORDER_REPLICATE, 2 = BORDER_REFLECT.\n\
 \t<output_path> is an output file name.\n\
 ";
@@ -25,7 +25,7 @@ int proccesArgument(int argc, char* argv[], string& image_path,
 Mat parse_kernel_from_txt(const string& kernel_path);
 
 double filter(const Mat& image, Mat& dst, const Mat& kernel, const Point& anchor,
-           int border_type, const string& output_name);
+              int border_type, const string& output_name);
 
 
 int main(int argc, char* argv[])
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
     string image_path, kernel_path, output_path;
     int x, y, border_type;
     if (proccesArgument(argc, argv, image_path, kernel_path, x, y,
-            border_type, output_path) != 0)
+                        border_type, output_path) != 0)
     {
         cout << helper << endl;
         return 1;
@@ -45,7 +45,7 @@ int main(int argc, char* argv[])
     Mat kernel = parse_kernel_from_txt(kernel_path);
 
     double elapsed_seconds = filter(image, dst, kernel, anchor,
-                                 border_type, output_path);
+                                    border_type, output_path);
 
     imwrite(output_path, dst);
     std::cout << "elapsed time: " << elapsed_seconds << "s\n";
@@ -116,7 +116,7 @@ Mat parse_kernel_from_txt(const string& kernel_path)
 }
 
 double filter(const Mat& image, Mat& dst, const Mat& kernel, const Point& anchor,
-           int border_type, const string& output_name)
+              int border_type, const string& output_name)
 {
     auto start = std::chrono::steady_clock::now();
     filter2D(image, dst, -1, kernel, anchor, 0, border_type);
