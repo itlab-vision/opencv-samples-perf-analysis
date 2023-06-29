ba79fed9b860672107b9b1f645aaa93697db225a
valentina-kustikova <valentina.kustikova.gmail.com>
2023-06-29 15:20:21 +0300

    Add infrastructure for utils

---
+++ utils/check_accuracy.cpp
@@ -0,0 +1,8 @@
+const char* helper = "./check_accuracy <predictions> <labels>\
+\t<predictions> is a file contained predicted class identifiers\
+\t<labels> is a file contained labeled class identifiers"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
---
+++ utils/diff_images.cpp
@@ -0,0 +1,8 @@
+const char* helper = "./diff_images <image1> <image2>\
+\t<image1> is the first image.\
+\t<image2> is the second image."
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
