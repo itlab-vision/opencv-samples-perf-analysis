d82484b71229a0fc20ff4b9387fa48ce0110be25
rodimkov <rodimkov@bk.ru>
2023-07-06 09:58:40 +0300

    fixed check svm acc

--- utils/check_svm_accuracy.cpp
+++ utils/check_svm_accuracy.cpp
@@ -3,27 +3,37 @@
 
 #include <opencv2/opencv.hpp>
 
+#include "../reader/CifarReader.h"
+
 using namespace std;
 using namespace cv;
 
 const char* helper =
-"./check_svm_accuracy <file>\n\
-\t<file> is a file contained labeled and predicted class identifiers\n\
+"./check_svm_accuracy <dataset> <predictions> <labels>\n\
+\t<dataset> is a string containing one of the supported datasets: <cifar10>.\n\
+\t<predictions> is a file contained predicted class identifiers.\n\
+\t<labels> is a file contained labeled class identifiers.\n\
 ";
 
 int main(int argc, char** argv)
 {
-    if (argc < 2)
+    Mat outputs, labels;
+    const map<string, BaseReader*> readers = {
+        {"cifar10", new CifarReader()}
+    };
+
+    if (argc < 4)
     {
         cout << helper;
         return 1;
     }
 
-    Mat outputs, labels;
-    FileStorage fs_read(argv[1], FileStorage::READ);
+    FileStorage fs_read(argv[2], FileStorage::READ);
     fs_read["outputs"] >> outputs;
-    fs_read["labels"] >> labels;
     fs_read.release();
+
+    BaseReader* reader = readers.at(argv[1]);
+    reader->load_test_labels(argv[3], labels);
 
     int number_correct = 0;
     for (int i = 0; i < outputs.rows; i++)
@@ -31,6 +41,7 @@ int main(int argc, char** argv)
         if (outputs.at<int>(0, i) == labels.at<int>(0, i))
             number_correct++;
     }
+
     cout << "accuracy: " << (float)number_correct / outputs.rows;
     return 0;
 }
